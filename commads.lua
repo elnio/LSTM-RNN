5e5af89772f120583e855a881300bb6768dd41ad
Lefteris Soulas <elnio@172-17-49-154.DYNAPOOL.NYU.EDU>
2015-04-10 20:36:13 -0400

    adding commands file

---
+++ commads.lua
@@ -0,0 +1,178 @@
+ok,cunn = pcall(require, 'fbcunn')
+if not ok then
+    ok,cunn = pcall(require,'cunn')
+    if ok then
+        print("warning: fbcunn not found. Falling back to cunn") 
+        LookupTable = nn.LookupTable
+    else
+        print("Could not find cunn or fbcunn. Either is required")
+        os.exit()
+    end
+else
+    deviceParams = cutorch.getDeviceProperties(1)
+    cudaComputeCapability = deviceParams.major + deviceParams.minor/10
+    LookupTable = nn.LookupTable
+end
+require('nngraph')
+require('base')
+g_init_gpu(arg)
+
+ptb = require('data')
+ptb2 = require('preprocessForSentimentAnalysis')
+params = {batch_size=21,
+                seq_length=21,
+                layers=2,
+                decay=2,
+                rnn_size=200,
+                dropout=0,
+                init_weight=0.1,
+                lr=1,
+                vocab_size=10000,
+                max_epoch=4,
+                max_max_epoch=13,
+                max_grad_norm=5}
+function transfer_data(x)
+  return x:cuda()
+end
+state_train, state_valid, state_test
+model = {}
+paramx, paramdx
+function lstm(i, prev_c, prev_h)
+  local function new_input_sum()
+    local i2h            = nn.Linear(params.rnn_size, params.rnn_size)
+    local h2h            = nn.Linear(params.rnn_size, params.rnn_size)
+    return nn.CAddTable()({i2h(i), h2h(prev_h)})
+  end
+  local in_gate          = nn.Sigmoid()(new_input_sum())
+  local forget_gate      = nn.Sigmoid()(new_input_sum())
+  local in_gate2         = nn.Tanh()(new_input_sum())
+  local next_c           = nn.CAddTable()({
+    nn.CMulTable()({forget_gate, prev_c}),
+    nn.CMulTable()({in_gate,     in_gate2})
+  })
+  local out_gate         = nn.Sigmoid()(new_input_sum())
+  local next_h           = nn.CMulTable()({out_gate, nn.Tanh()(next_c)})
+  return next_c, next_h
+end
+function create_network()
+  local x                = nn.Identity()()
+  local y                = nn.Identity()()
+  local prev_s           = nn.Identity()()
+  local i                = {[0] = LookupTable(params.vocab_size,
+                                                    params.rnn_size)(x)}
+  local next_s           = {}
+  local split         = {prev_s:split(2 * params.layers)}
+  for layer_idx = 1, params.layers do
+    local prev_c         = split[2 * layer_idx - 1]
+    local prev_h         = split[2 * layer_idx]
+    local dropped        = nn.Dropout(params.dropout)(i[layer_idx - 1])
+    local next_c, next_h = lstm(dropped, prev_c, prev_h)
+    table.insert(next_s, next_c)
+    table.insert(next_s, next_h)
+    i[layer_idx] = next_h
+  end
+  local h2y              = nn.Linear(params.rnn_size, params.vocab_size)
+  local dropped          = nn.Dropout(params.dropout)(i[params.layers])
+  local pred             = nn.LogSoftMax()(h2y(dropped))
+  local err              = nn.ClassNLLCriterion()({pred, y})
+  local module           = nn.gModule({x, y, prev_s},
+                                      {err, nn.Identity()(next_s)})
+  module:getParameters():uniform(-params.init_weight, params.init_weight)
+  return transfer_data(module)
+end
+function setup()
+  print("Creating a RNN LSTM network.")
+  local core_network = create_network()
+  paramx, paramdx = core_network:getParameters()
+  model.s = {}
+  model.ds = {}
+  model.start_s = {}
+  for j = 0, params.seq_length do
+    model.s[j] = {}
+    for d = 1, 2 * params.layers do
+      model.s[j][d] = transfer_data(torch.zeros(params.batch_size, params.rnn_size))
+    end
+  end
+  for d = 1, 2 * params.layers do
+    model.start_s[d] = transfer_data(torch.zeros(params.batch_size, params.rnn_size))
+    model.ds[d] = transfer_data(torch.zeros(params.batch_size, params.rnn_size))
+  end
+  model.core_network = core_network
+  model.rnns = g_cloneManyTimes(core_network, params.seq_length)
+  model.norm_dw = 0
+  model.err = transfer_data(torch.zeros(params.seq_length))
+end
+function reset_state(state)
+  state.pos = 1
+  if model ~= nil and model.start_s ~= nil then
+    for d = 1, 2 * params.layers do
+      model.start_s[d]:zero()
+    end
+  end
+end
+function reset_ds()
+  for d = 1, #model.ds do
+    model.ds[d]:zero()
+  end
+end
+step = 0
+epoch = 0
+total_cases = 0
+words_per_step = params.seq_length * params.batch_size
+
+
+---------------------------------- IMPORTANT FUNCTIONS ----------------------------------
+function fp(state)
+  g_replace_table(model.s[0], model.start_s)
+  if state.pos + params.seq_length > state.data:size(1) then
+    reset_state(state)
+  end
+  for i = 1, params.seq_length do
+    local x = state.data[state.pos]
+    local y = state.data[state.pos + 1]
+    local s = model.s[i - 1]
+    model.err[i], model.s[i] = unpack(model.rnns[i]:forward({x, y, s}))
+    state.pos = state.pos + 1
+  end
+  g_replace_table(model.start_s, model.s[params.seq_length])
+  return model.err:mean()
+end
+
+function bp(state)
+  paramdx:zero()
+  reset_ds()
+  for i = params.seq_length, 1, -1 do
+    state.pos = state.pos - 1
+    local x = state.data[state.pos]
+    local y = state.data[state.pos + 1]
+    local s = model.s[i - 1]
+    local derr = transfer_data(torch.ones(1))
+    local tmp = model.rnns[i]:backward({x, y, s},
+                                       {derr, model.ds})[3]
+    g_replace_table(model.ds, tmp)
+    cutorch.synchronize()
+  end
+  state.pos = state.pos + params.seq_length
+  model.norm_dw = paramdx:norm()
+  if model.norm_dw > params.max_grad_norm then
+    local shrink_factor = params.max_grad_norm / model.norm_dw
+    paramdx:mul(shrink_factor)
+  end
+  paramx:add(paramdx:mul(-params.lr))
+end
+
+
+
+
+---------------------------------- SERIOUS STUFF ----------------------------------
+
+state_train = {data=transfer_data( ptb.traindataset(params.batch_size) )}
+--state_train = {data=transfer_data( load_csv() )}
+
+reset_state(state_train)
+setup()
+
+fp(state_train)
+
+
+
